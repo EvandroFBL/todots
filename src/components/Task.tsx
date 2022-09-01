e3afc789c40dbcf3d8248ffeec463b7e8fc856da
EvandroFBL <evandrofbl@gmail.com>
2022-08-31 22:27:40 -0300

    styling

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -5,9 +5,11 @@ export function Task() {
     
     return (
         <div className={styles.task}>
-            <input type="checkbox" name="taskCheck" id="taskCheck" />
+            <input type="checkbox" name="taskCheck" id="taskCheck" className={styles.taskCheck} />
             <span className={styles.taskDescription}>Let's just make it and leave.</span>
-            <Trash size={16} />
+            <div className={styles.taskDelete}>
+                <Trash size={16} />
+            </div>
         </div>
     );
 }
